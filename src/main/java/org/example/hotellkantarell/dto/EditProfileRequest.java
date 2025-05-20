e419861486f4b14b98818b03ffdf5568b03b5795
Isak Folke <isvack@gmail.com>
2025-05-20 10:53:47 +0200

    Tog bort BookingController

--- src/main/java/org/example/hotellkantarell/dto/EditProfileRequest.java
+++ src/main/java/org/example/hotellkantarell/dto/EditProfileRequest.java
@@ -1,7 +1,7 @@
 package org.example.hotellkantarell.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
 
 
