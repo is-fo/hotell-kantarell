fc5ccd062ab54acd936d1ac8ebe060879f9def4e
Isak Folke <isvack@gmail.com>
2025-05-21 12:56:24 +0200

    Fel och success-meddelanden visas nu. Det är inte inlagt så att man får det på alla sidor, men htmlen och javascripten finns för alla sidor.

--- src/main/java/org/example/hotellkantarell/controller/UserController.java
+++ src/main/java/org/example/hotellkantarell/controller/UserController.java
@@ -7,9 +7,11 @@ import org.example.hotellkantarell.model.User;
 import org.example.hotellkantarell.repository.UserRepository;
 import org.example.hotellkantarell.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -26,8 +28,15 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute RegisterRequest registerRequest) {
-        return userService.register(registerRequest) != null ? "redirect:/login" : "redirect:/register";
+    public String registerUser(@ModelAttribute RegisterRequest registerRequest, RedirectAttributes redirectAttributes, Model model) {
+        User user = userService.register(registerRequest);
+        if (user == null) {
+            model.addAttribute("error", "Det finns redan ett konto med den emailen eller så är det kaffe i servern.");
+            return "register";
+        } else {
+            redirectAttributes.addFlashAttribute("success", "Du är nu registrerad!");
+            return "redirect:/login";
+        }
     }
 
     @GetMapping("/login")
@@ -36,17 +45,15 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginUser(@ModelAttribute LoginRequest loginRequest, HttpSession session) {
+    public String loginUser(@ModelAttribute LoginRequest loginRequest, RedirectAttributes redirectAttributes, HttpSession session) {
         User user = userService.login(loginRequest);
         if (user != null) {
             session.setAttribute("user", user);
-            return "start";
+            redirectAttributes.addFlashAttribute("success", "Välkommen " + user.getName() + "!");
+            return "redirect:/start";
         }
+        redirectAttributes.addFlashAttribute("error", "Fel användarnamn eller lösenord");
         return "redirect:/login";
     }
-
-
-
-
 
 }
