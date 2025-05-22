c8e0ac3a4c985cda102e8e51e5bd242e0568363e
Joachim Wiltman <jke@live.se>
2025-05-22 09:14:53 +0200

    Ändrade isaks ENUM...............

--- src/main/java/org/example/hotellkantarell/status/BookingStatus.java
+++ src/main/java/org/example/hotellkantarell/status/BookingStatus.java
@@ -1,14 +1,14 @@
 package org.example.hotellkantarell.status;
 
 public enum BookingStatus {
-    NO_SUCH_BOOKING("KAFFE I SERVERN FINNS INGEN SÅN BOKNING KALLA PÅ ALLA DATABASTECKNINKER"),
-    MALFORMED_BOOKING_USER("The booking has no user attached."),
-    MALFORMED_BOOKING_ROOM("The room has no room attached."),
-    EXPIRED_DATE("The date is in the past."),
-    REVERSE_DATE("The start date is after the end date."),
-    DOUBLE_BOOKED("The room is already booked."),
-    ILLEGAL_ACCESS("yea we calling the fkn cops on u"),
-    SUCCESS("yaaaaaaaaahhooooooooo");
+    NO_SUCH_BOOKING("Kunde inte hitta bokningen."),
+    MALFORMED_BOOKING_USER("Något gick fel, bokningen har ingen användare."),
+    MALFORMED_BOOKING_ROOM("Bokningen information om saknar rum."),
+    EXPIRED_DATE("Datum är i dåtid"),
+    REVERSE_DATE("Incheckningsdatum är efter utcheckningsdatum."),
+    DOUBLE_BOOKED("Rummet är redan bokat."),
+    ILLEGAL_ACCESS("Åtkomst nekad."),
+    SUCCESS("Bokningen lyckades!");
 
     private final String message;
 
